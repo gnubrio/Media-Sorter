171c6f04016fe212bbcd8511997d5e1c7fb3bc2d
gnubrio <whitecc2005@proton.me>
2024-11-03 18:10:17 -0800

    mediasorter

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import cv2
 
 class App(tk.Tk):
     def __init__(self):
-        # Application parameters
         super().__init__()
         self.title("Media Sorter")
         self.minsize(600, 600)
@@ -24,18 +23,17 @@ class App(tk.Tk):
         )
         self.SUPPORTED_VIDEOS = (".mp4", ".mov")
 
-        # Media files and directory paths/buttons storage
         self.media_files = []
         self.current_media_index = 0
         self.destination_paths = []
         self.directory_buttons = []
+        self.undo_stack = []
 
         self.setup_UI()
 
         self.mainloop()
 
     def setup_UI(self):
-        # Display widgets
         media_display_frame = ttk.Frame(self, border=2, relief=tk.SOLID)
         media_display_frame.place(x=0, y=0, relwidth=0.9, relheight=0.4)
 
@@ -45,7 +43,6 @@ class App(tk.Tk):
         self.media_remaining_label = ttk.Label(media_display_frame, text=None)
         self.media_remaining_label.pack(side=tk.RIGHT)
 
-        # Menu widgets
         menu_button_frame = ttk.Frame(self, border=2, relief=tk.SOLID)
         menu_button_frame.place(relx=0.9, y=0, relwidth=0.1, relheight=1)
 
@@ -83,16 +80,13 @@ class App(tk.Tk):
         )
         previous_media_button.grid(row=4, column=0, sticky=tk.NSEW, padx=5, pady=10)
 
-        # Directory widgets
         self.directory_button_frame = ttk.Frame(self, border=2, relief=tk.SOLID)
         self.directory_button_frame.place(x=0, rely=0.4, relwidth=0.9, relheight=0.6)
 
     def load_media(self):
-        # Reset media files
         self.media_files = []
         self.current_media_index = 0
 
-        # Load all media files in directory and subdirectories
         directory = filedialog.askdirectory(title="Select Folder")
 
         if os.path.isdir(directory):
@@ -107,21 +101,18 @@ class App(tk.Tk):
         self.display_media()
 
     def display_media(self):
-        # Repack media labels and find current index
         self.media_display_label.pack()
         self.media_remaining_label.config(text=f"Remaining: {len(self.media_files)}")
         self.media_remaining_label.pack()
         media = self.media_files[self.current_media_index]
         max_height = 400
 
-        # Resize and display image files
         if media.lower().endswith(self.SUPPORTED_IMAGES):
             pillow_heif.register_heif_opener()
             img = Image.open(media)
             img.thumbnail((10000, max_height))
             self.img_tk = ImageTk.PhotoImage(image=img)
 
-        # Resize and display video files
         elif media.lower().endswith(self.SUPPORTED_VIDEOS):
             video = cv2.VideoCapture(media)
             ret, frame = video.read()
@@ -135,12 +126,10 @@ class App(tk.Tk):
             frame_resized = cv2.resize(frame_rgb, (new_width, new_height))
             self.img_tk = ImageTk.PhotoImage(image=Image.fromarray(frame_resized))
 
-        # Apply media to media label
         self.media_display_label.config(image=self.img_tk)
         self.update_idletasks()
 
     def change_media(self, direction):
-        # Seek through media in media array
         if direction == "next" and self.current_media_index < len(self.media_files) - 1:
             self.current_media_index += 1
             self.display_media()
@@ -151,7 +140,6 @@ class App(tk.Tk):
             return
 
     def move_media(self, index):
-        # Move media and remove from media array
         if len(self.media_files) > 0:
             shutil.move(
                 self.media_files[self.current_media_index],
@@ -159,7 +147,6 @@ class App(tk.Tk):
             )
             del self.media_files[self.current_media_index]
 
-            # Display next, previous, or no media
             if len(self.media_files) == 0:
                 self.media_display_label.pack_forget()
             elif self.current_media_index == len(self.media_files):
@@ -170,16 +157,13 @@ class App(tk.Tk):
             return
 
     def add_destinations(self):
-        # Add subdirectories if possible, if not add selected directory
         directory = filedialog.askdirectory(title="Select Folder")
 
         if directory:
-            # Search for sub directories
             if any(
                 os.path.isdir(os.path.join(directory, content))
                 for content in os.listdir(directory)
             ):
-                # Add subdirectories
                 for content in os.listdir(directory):
                     content_path = os.path.join(directory, content)
                     if os.path.isdir(content_path):
@@ -188,30 +172,25 @@ class App(tk.Tk):
                             self.destination_paths.append(path)
                         else:
                             print(f"{content} already loaded")
-            # Add selected directory if not subdirectories
             else:
                 if directory not in self.destination_paths:
                     self.destination_paths.append(directory)
                 else:
                     print(f"{directory} already loaded")
 
-        # Create buttons for directories
         if len(self.destination_paths) != 0:
             self.create_directory_buttons()
 
     def create_directory_buttons(self):
-        # Setup buttons with amount of directories provided
         max_columns = 10
         button_count = len(self.destination_paths)
         row_count = (button_count + max_columns - 1) // max_columns
 
-        # Sort basename of directories
         directory_names = [
             (os.path.basename(path), i) for i, path in enumerate(self.destination_paths)
         ]
         sorted_directories = sorted(directory_names, key=lambda x: x[0].lower())
 
-        # Create buttons
         for i, (directory_name, index) in enumerate(sorted_directories):
             directory_button = ttk.Button(
                 self.directory_button_frame,
@@ -228,14 +207,12 @@ class App(tk.Tk):
 
             self.directory_buttons.append(directory_button)
 
-        # Configure buttons
         for i in range(row_count):
             self.directory_button_frame.rowconfigure(i, weight=1)
         for i in range(max_columns):
             self.directory_button_frame.columnconfigure(i, weight=1)
 
     def clear_directory_buttons(self):
-        # Clear all directories
         for button in self.directory_buttons:
             button.destroy()
 
